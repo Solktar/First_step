2b33f7d33603dbc40a54b93e15fdd7f5404819c1
Solktar <Leyter922@gmail.com>
2020-10-19 22:17:06 +0300

    Добавил отсечку на цифры меньше 1 и больше 10.

--- src/Task_5.java
+++ src/Task_5.java
@@ -6,8 +6,12 @@ public class Task_5 {
         System.out.println("Сейчас таблицу умножения показывать буду.");
         System.out.println("Введи число, от 1 до 10.");
         byte i = sc.nextByte();
+        if (i<1 | i>10){
+            System.out.println("Нет, так не пойдёт.");
+        }
+        if (i>=1 & i<=10){
         System.out.println("Держи: \n" +
-                i + "x1=" + i * 1 + "\n" +
+                i + "x1=" + i + "\n" +
                 i + "x2=" + i * 2 + "\n" +
                 i + "x3=" + i * 3 + "\n" +
                 i + "x4=" + i * 4 + "\n" +
@@ -16,7 +20,7 @@ public class Task_5 {
                 i + "x7=" + i * 7 + "\n" +
                 i + "x8=" + i * 8 + "\n" +
                 i + "x9=" + i * 9 + "\n" +
-                i + "x10=" + i * 10 + "\n");
+                i + "x10=" + i * 10 + "\n");}
 
     }
 }

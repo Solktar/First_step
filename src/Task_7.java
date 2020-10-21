e769fe2b19ce9cbd4047755f38ea043c7baef56d
Solktar <Leyter922@gmail.com>
2020-10-21 12:58:06 +0300

    Поправил задание 7, переменные на константы.

--- src/Task_7.java
+++ src/Task_7.java
@@ -1,20 +1,30 @@
 import java.util.Scanner;
 
 public class Task_7 {
+    public static int X = 33;
+    public static int Y = 44;
+    public static int Z = 55;
+
     public static void main(String[] args) {
         System.out.println("Введите целое число");
         Scanner sc = new Scanner(System.in);
-        int x, y, z, number;
-        x = 1;
-        y = 2;
-        z = 44;
-        number = sc.nextInt();
-        if (number == x || number == y || number == z) {
+        int number = sc.nextInt();
+        if (number == X || number == Y || number == Z) {
             System.out.println("Данное значение имеется в константах");
         } else {
             System.out.println("Такой константы нет!");
         }
 
+/*        switch (number) {
+            case X:
+            case Y:
+            case Z:
+                System.out.println("Данное значение имеется в константах");
+                break;
+            default:
+                System.out.println("Такой константы нет!");
+                break;
+        }
+*/
     }
-
 }
